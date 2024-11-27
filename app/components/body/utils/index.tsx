fbd948b395484e0ffbc669b9c6ba9fddb1481449
lauraboemo <lauraboemo@gmail.com>
2024-11-26 22:23:12 -0300

    fix pnpm

--- app/components/body/utils/index.tsx
+++ app/components/body/utils/index.tsx
@@ -2,4 +2,3 @@ export * from "./handleS3Upload";
 export * from "./handleS3Delete";
 export * from "./handleAIUpload";
 export * from "./handleAIReading";
-export * from "./handleGoogleCloudTranslation";
