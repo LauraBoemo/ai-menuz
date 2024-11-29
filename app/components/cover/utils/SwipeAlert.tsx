df7f5a46781c97ebe6a558933b48d59c3ea4c4ee
lauraboemo <lauraboemo@gmail.com>
2024-11-29 19:55:50 -0300

    [UPDATE] Add Swipe Animation

--- app/components/cover/utils/SwipeAlert.tsx
+++ app/components/cover/utils/SwipeAlert.tsx
@@ -1,13 +1,30 @@
 import { Stack, Typography } from "@mui/material";
-import SwipeIcon from '@mui/icons-material/Swipe';
+import SwipeIcon from "@mui/icons-material/Swipe";
+import { keyframes } from "@emotion/react";
+import { styled } from "@mui/material/styles";
+
+const swipeAnimation = keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(20px);
+  }
+`;
+
+const AnimatedSwipeIcon = styled(SwipeIcon)(({ theme }) => ({
+  color: theme.palette.primary.light,
+  marginLeft: "-10px",
+  animation: `${swipeAnimation} 1.5s ease-in-out infinite`,
+}));
 
 export const SwipeAlert = () => {
-  return ( 
+  return (
     <Stack alignItems={"center"} gap={1}>
-      <SwipeIcon style={{ color: "primary.light" }} />
+      <AnimatedSwipeIcon />
       <Typography variant={"body1"}>Swipe to navigate!</Typography>
-    </Stack>    
+    </Stack>
   );
-}
+};
 
 export default SwipeAlert;
